d324d9bd275ac0801886d40ac3027f66b738cb09
Maksym Miroshnichenko <mminqusitor@gmail.com>
2020-12-14 12:59:29 +0200

    First commit

---
+++ js/Index.js
@@ -0,0 +1,7 @@
+const firstNumber = Number(prompt('Enter the first number.'));
+const secondNumber = Number(prompt('Enter the second number.'));
+
+alert('sum: ' + firstNumber + ' + ' + secondNumber + ' = ' + (firstNumber + secondNumber));
+alert('subtraction: ' + firstNumber + ' - ' + secondNumber + ' = ' + (firstNumber - secondNumber));
+alert('Multiplication: ' + firstNumber + ' * ' + secondNumber + ' = ' + (firstNumber * secondNumber));
+alert('division: ' + firstNumber + ' / ' + secondNumber + ' = ' + (firstNumber / secondNumber));

eb840735e92cbd42fc49e2bbe9d8d603134e65d1
Maksym Miroshnichenko <mminqusitor@gmail.com>
2020-12-21 17:02:53 +0200

    W5

--- js/index5.js
+++ js/index5.js
@@ -1,8 +1,11 @@
 const inputNumber = parseInt(prompt('Insert the number '));
 
 for (let i = 1; i <= inputNumber; i = i * 3) {
+    Cannot = (inputNumber / i) % 1 != 0;
     if (i === inputNumber) {
         alert(`${inputNumber}  - This number can be obtained by raising 3 to the power`);
+    } else if (Cannot) {
+        alert(`${inputNumber} - Cannot be obtained by raising 3 to a power`);
     }
 }
 

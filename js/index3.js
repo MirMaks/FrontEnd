0161eae6dea04138ab60cb42fab2a6b2f0df5dce
MirMaks <75981411+MirMaks@users.noreply.github.com>
2020-12-23 20:48:40 +0200

    Delete index3.js

--- js/index3.js
+++
@@ -1,8 +0,0 @@
-const integer = parseInt(prompt('Please enter an integer '));
-const messange = `All integers from 1 to 100 whose square does not exceed the number ${integer}`
-console.log(messange);
-for (i = 1; i <= 100; i++) {
-    if (Math.pow(i, 2) <= integer) {
-        console.log(i);
-    };
-}

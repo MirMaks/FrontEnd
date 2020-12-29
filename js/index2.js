527d70de4ecbfe746cee79f2aeea0cdf126e1730
Maksym Miroshnichenko <mminqusitor@gmail.com>
2020-12-29 15:58:18 +0200

    hw5

--- js/index2.js
+++ js/index2.js
@@ -1,15 +1,15 @@
 const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-let arrItemSum = [];
 let arrItemPlus = [];
 let arrItemMin = [];
 let arrItemMinOne = [];
 let arrItemMax = [];
 let oddPositives = [];
 let evenPositives = [];
-let oddPositivesSum = [];
-let evenPositivesSum = [];
-let productOfPositive = [];
+let sumEven = 0;
+let sumOdd = 0;
+let productPositive = 1;
 let zeroing = [];
+let sum = 0;
 console.log(arr + ' -  Array')
 console.log('Positive array elements:')
 for (let i = 0; i <= arr.length; i++) {
@@ -18,26 +18,20 @@ for (let i = 0; i <= arr.length; i++) {
     }
 }
 console.log(arrItemPlus);
-arrItemSum = arrItemPlus[0] + arrItemPlus[1] + arrItemPlus[2] + arrItemPlus[3] + arrItemPlus[4] + arrItemPlus[5] + arrItemPlus[6] + arrItemPlus[7] + arrItemPlus[8] + arrItemPlus[9] + arrItemPlus[10] + arrItemPlus[11] + arrItemPlus[12] + arrItemPlus[13];
-console.log(arrItemSum + ' - Sum of all positive elements');
+for (let i = 0; i < arrItemPlus.length; i++) {
+    sum += arrItemPlus[i];
+}
+console.log(sum + ' - Sum of all positive elements');
 for (let i = 0; i <= arr.length; i++) {
     if (arr[i] < 0) {
         arrItemMin.push(arr[i]);
     }
 }
-for (let j = 0; j <= arrItemMin.length; j++) {
-    if (arrItemMin[j] < -62) {
-        arrItemMinOne.push(arrItemMin[j]);
-    }
-}
+const min = Math.min(...arrItemMin);
 console.log(arrItemMin + ' - Negative numbers');
-console.log(arrItemMinOne + ' - Minimum number, its index - ' + arr.indexOf(-63));
-for (let i = 0; i <= arrItemPlus.length; i++) {
-    if (arrItemPlus[i] > 72) {
-        arrItemMax.push(arrItemPlus[i]);
-    }
-}
-console.log(arrItemMax + ' - Maximum number, its index - ' + arr.indexOf(76));
+console.log(min + ' - Minimum number, its index - ' + arr.indexOf(min));
+const max = Math.max(...arrItemPlus);
+console.log(max + ' - Maximum number, its index - ' + arr.indexOf(76));
 for (i = 0; i <= arrItemPlus.length; i++) {
     if (arrItemPlus[i] % 2 == 1) {
         oddPositives.push(arrItemPlus[i]);
@@ -51,20 +45,26 @@ for (i = 0; i <= arrItemPlus.length; i++) {
 }
 console.log(evenPositives + ' - even positive elements');
 
-evenPositivesSum = evenPositives[0] + evenPositives[1] + evenPositives[2] + evenPositives[3] + evenPositives[4] + evenPositives[5] + evenPositives[6] + evenPositives[7] + evenPositives[8] + evenPositives[9];
-console.log(evenPositivesSum + ' - sum even positive elements');
+for (let i = 0; i < evenPositives.length; i++) {
+    sumEven += evenPositives[i];
+}
+console.log(sumEven + ' - sum even positive elements');
+for (i = 0; i < oddPositives.length; i++) {
+    sumOdd += oddPositives[i]
+}
+console.log(sumOdd + ' - sum odd positive elements');
 
-oddPositivesSum = oddPositives[0] + oddPositives[1] + oddPositives[2] + oddPositives[3];
-console.log(oddPositivesSum + ' - sum odd positive elements');
-
-productOfPositive = arrItemPlus[0] * arrItemPlus[1] * arrItemPlus[2] * arrItemPlus[3] * arrItemPlus[4] * arrItemPlus[5] * arrItemPlus[6] * arrItemPlus[7] * arrItemPlus[8] * arrItemPlus[9] * arrItemPlus[10] * arrItemPlus[11] * arrItemPlus[12] * arrItemPlus[13];
-console.log(productOfPositive + ' - Product of positive elements');
+for (i = 0; i < arrItemPlus.length; i++) {
+    productPositive *= arrItemPlus[i];
+}
+console.log(productPositive + ' - Product of positive elements');
 
 for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] > 72) {
+    if (arr[i] >= max) {
         zeroing.push(arr[i]);
-    } else if (arr[i] < 76) {
-        zeroing.push(arr[i] * 0);
+    } else if (arr[i] < max) {
+        zeroing.push(arr[i] = 0);
     }
 }
 console.log(zeroing + ' - Maximum number and zeroing');
+

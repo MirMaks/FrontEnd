1213ce8ea3a2476740521eee27e0e7203a8a8acc
Maksym Miroshnichenko <mminqusitor@gmail.com>
2020-12-29 16:05:39 +0200

    hw5

--- js/index2.js
+++ js/index2.js
@@ -1,8 +1,6 @@
 const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 let arrItemPlus = [];
 let arrItemMin = [];
-let arrItemMinOne = [];
-let arrItemMax = [];
 let oddPositives = [];
 let evenPositives = [];
 let sumEven = 0;
